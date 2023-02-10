7bdae7fc236ec7e02e9fd942faac55ea43f56bc0
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-10 14:07:16 -0300

    Product

---
+++ src/main/java/com/aulaSpringBoot/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.aulaSpringBoot.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aulaSpringBoot.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
