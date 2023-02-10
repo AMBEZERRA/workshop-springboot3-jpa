7ded9dcea43b1e6f912e3d1c081405eb2a17b2ee
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-10 12:49:07 -0300

    Order Status enum

--- src/main/java/com/aulaSpringBoot/course/entities/Order.java
+++ src/main/java/com/aulaSpringBoot/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.aulaSpringBoot.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -25,7 +26,10 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
+	
 	// asociação
 	@ManyToOne
 	@JoinColumn(name = "Client_id")
@@ -35,13 +39,15 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
+	
 	public Long getId() {
 		return id;
 	}
@@ -64,6 +70,17 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null)
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
